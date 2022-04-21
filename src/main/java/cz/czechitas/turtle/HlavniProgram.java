b4e843d00d52245f1c33c44ebd546db608b688e0
Kateřina Petrášková <ka.petraskova@gmail.com>
2022-04-21 13:19:07 +0200

    1. cast ukolu - metody s parametry pro: rovnostranny trojuhelnik, ctverec, kolecko a obdelnik

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,12 +2,78 @@ package cz.czechitas.turtle;
 
 import cz.czechitas.turtle.engine.*;
 
+import java.awt.*;
+
 public class HlavniProgram {
 
     public void main(String[] args) {
         Turtle zofka;
-
         zofka = new Turtle();
+
+        // priprava zofky na kresleni
+        zofka.setLocation(100, 100);
+        zofka.setPenWidth(5);
+
+        //ctverec
+        nakresliCtverec(zofka, 50.0, Color.orange);
+
+        // nova zelva tyna
+        Turtle tyna;
+        tyna = new Turtle();
+        tyna.setLocation(250, 250);
+        tyna.setPenColor(Color.RED);
+        tyna.setPenWidth(5);
+
+        //obdelnik
+        nakresliObdelnik(tyna, 50.0, 75.0, Color.pink);
+
+        //zofka jde kreslit trojuhelnik
+        zofka.setLocation(100, 300);
+
+        //rovnostranny trojuhelnik
+        nakresliRovnostrannyTrojuhelnik(zofka, 80.0, Color.gray);
+
+        //priprava tyny na kresleni kolecka
+        tyna.setLocation(450, 200);
+
+        //kolecko
+        nakresliKolecko(tyna, 10.0, Color.MAGENTA);
+
+
+    }
+
+    private void nakresliKolecko(Turtle turtle, double delkaStrany, Color barva) {
+        turtle.setPenColor(barva);
+        for (int l = 0; l < 36; l++) {
+            turtle.move(delkaStrany);
+            turtle.turnRight(10);
+        }
+    }
+
+    private void nakresliRovnostrannyTrojuhelnik(Turtle turtle, double delkaStrany, Color barva) {
+        turtle.setPenColor(barva);
+        for (int k = 0; k < 3; k++) {
+            turtle.move(delkaStrany);
+            turtle.turnRight(120);
+        }
+    }
+
+    private void nakresliObdelnik(Turtle turtle, double prvniStrana, double druhaStrana, Color barva) {
+        turtle.setPenColor(barva);
+        for (int j = 0; j < 2; j++) {
+            turtle.move(prvniStrana);
+            turtle.turnRight(90);
+            turtle.move(druhaStrana);
+            turtle.turnRight(90);
+        }
+    }
+
+    private void nakresliCtverec(Turtle turtle, double delkaStrany, Color barva) {
+        turtle.setPenColor(barva);
+        for (int i = 0; i < 4; i++) {
+            turtle.move(delkaStrany);
+            turtle.turnRight(90);
+        }
     }
 
 }
